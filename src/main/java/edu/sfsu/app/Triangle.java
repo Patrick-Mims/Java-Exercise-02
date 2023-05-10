0f7b0d57cb53379b8045cc5b85ce12a8f60d590a
weatherpub <patrick@bayareamedia.net>
2023-05-09 20:43:39 -0700

    Latest Files

---
+++ src/main/java/edu/sfsu/app/Triangle.java
@@ -0,0 +1,30 @@
+package edu.sfsu.app;
+
+public class Triangle extends GeometricObject {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle() {
+        this.side1 = 1.0;
+        this.side2 = 1.0;
+        this.side3 = 1.0;
+    }
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getArea() {
+        return (this.side1 * this.side2 * this.side3);
+    }
+
+    public double getParameter() {
+        return (this.side1 + this.side2 + this.side3);
+    }
+
+    public String toString() {
+        return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
+    }
+}
